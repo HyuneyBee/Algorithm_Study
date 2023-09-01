572e00320fcce2d6bbb210249d1e6f453f516765
HyuneyBee <gsh2448@gmail.com>
2023-09-01 22:37:03 +0900

    1676 해결

---
+++ java-version/src/main/java/baekjoon/problem1676.java
@@ -0,0 +1,18 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class problem1676 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int count = 0;
+        while (n >= 5) {
+            count += n / 5;
+            n /= 5;
+        }
+
+        System.out.println(count);
+    }
+}
